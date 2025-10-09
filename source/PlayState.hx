05df1f52d3e4148a49765e6b4291cc0111ef05c6
longge90 <151552445+longge90@users.noreply.github.com>
2025-10-09 21:43:16 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -31,6 +31,7 @@ import flixel.system.FlxSound;
 import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
+import flixel.tweens.misc.VarTween;
 import flixel.ui.FlxBar;
 import flixel.util.FlxCollision;
 import flixel.util.FlxColor;
